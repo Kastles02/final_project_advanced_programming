90145947f40014fa9f08bee69956ed322b673e8a
Kastles02 <k0473050@amarillocollege.com>
2025-12-08 10:21:36 -0600

    Add files via upload
    The start of the event manager

---
+++ event_manager.py
@@ -0,0 +1,22 @@
+import event
+def main_event():
+    events = []
+    user = input("Welcome to the Event Manager! Choose from the following menu options.\n1.Check Events\n2.Add Event\n3.Modify Event\n4.Delete Event")
+    if user == 1:
+        for item in event:
+                print(f"Name: {event.get_name()}")
+                print(f"Room Number: {student.get_room_num()}")
+                print(f"Club Name: {student.get_club_name()}")
+                print(f"Event Time: {student.get_event_time()}")
+                print(f"Average/Estimated Attendance: {student.get_avg_atten()}")
+                print(f"Contact Information: {student.get_contact_info()}")
+                print()
+    else if user = 2:
+        name = input("Enter the Event Name.")
+        room_num = input("Enter the Room Number the event will be in.")
+        club_name = input("Enter the Club Name.")
+        event_time = input("Enter the Event Time")
+        avg_atten = input("Enter the amount of students you expect to attend the Event.")
+        contact_info = input("Enter the Contact Information for the president or supervisor of the event.")
+        events.append(Event(name,room_num,club_name,event_time,avg_atten,contact_info))
+        
