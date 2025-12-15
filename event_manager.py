ed759c90d0f1dc92bb74eb8a90863916bdc00b06
Kastles02 <k0473050@amarillocollege.com>
2025-12-15 10:46:06 -0600

    Update event_manager.py

--- event_manager.py
+++ event_manager.py
@@ -1,26 +1,19 @@
-import event
-def main_event():
-    events = []
-    user = input("Welcome to the Event Manager! Choose from the following menu options.\n1.Check Events\n2.Add Event\n3.Modify Event\n4.Delete Event")
-    if user == 1:
-        for item in event:
-                print(f"Name: {event.get_name()}")
-                print(f"Room Number: {event.get_room_num()}")
-                print(f"Club Name: {event.get_club_name()}")
-                print(f"Event Time: {event.get_event_time()}")
-                print(f"Average/Estimated Attendance: {event.get_avg_atten()}")
-                print(f"Contact Information: {event.get_contact_info()}")
-                print()
-    elif user = 2:
-        name = input("Enter the Event Name.")
-        room_num = input("Enter the Room Number the event will be in.")
-        club_name = input("Enter the Club Name.")
-        event_time = input("Enter the Event Time")
-        avg_atten = input("Enter the amount of students you expect to attend the Event.")
-        contact_info = input("Enter the Contact Information for the president or supervisor of the event.")
-        events.append(Event(name,room_num,club_name,event_time,avg_atten,contact_info))
-    elif user = 3:
-        count = 0
-        for item in events:
+from file_manager import load_events, load_rooms, save_event
+from event import Event
+
+def event_conflict(room_num, date, time):
+    events = load_events()
+    for e in events:
+        if e.get_room_num() == room_num and e.get_date() == date and e.get_time() == time:
+            return True
+    return False
+
+def add_event(event):
+    if event_conflict(event.get_room_num(), event.get_date(), event.get_time()):
+        print("Scheduling conflict detected!")
+        return
+    save_event(event)
+    print("Event scheduled.")
+
 
 
